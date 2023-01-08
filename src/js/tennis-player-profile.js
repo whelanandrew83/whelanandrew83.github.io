917b761168f06072e0272096fd0b71c3bc183d2f
whelanandrew83 <64817711+whelanandrew83@users.noreply.github.com>
2023-01-08 19:17:20 +1100

    Updates

--- src/js/tennis-player-profile.js
+++ src/js/tennis-player-profile.js
@@ -2,10 +2,26 @@ const tableDataUrl = 'https://www.wheeloratings.com/src/player_profiles_tennis/t
 
 const compareColumns = {
     "Age": { name: "Age", dec: 0 },
+    "Matches": { name: "Matches", dec: 0 },
     "Wins": { name: "Wins", dec: 0 },
     "WinPercentage": { name: "Win %", dec: 1 },
     "RacePoints": { name: "Race Points", dec: 1 },
     "PointsWonPercentage": { name: "Points Won %", dec: 1 },
     "GamesWonPercentage": { name: "Games Won %", dec: 1 },
-    "SetsWonPercentage": { name: "Sets Won %", dec: 1 }
+    "SetsWonPercentage": { name: "Sets Won %", dec: 1 },
+    "FirstServePercentage": { name: "1st Serve %", dec: 1 },
+    "FirstServeWonPercentage": { name: "1st Serve Won %", dec: 1 },
+    "SecondServeWonPercentage": { name: "2nd Serve Won %", dec: 1 },
+    "ServicePointsWonPercentage": { name: "Service Points Won %", dec: 1 },
+    "ServiceGamesWonPercentage": { name: "Service Games Won %", dec: 1 },
+    "Aces": { name: "Aces", dec: 0 },
+    "AcePercentage": { name: "Ace %", dec: 1 },
+    "DoubleFaultPercentage": { name: "Double Fault %", dec: 1 },
+    "BreakPointsSavedPercentage": { name: "Break Points Saved %", dec: 1 },
+    "FirstServeReturnPointsWonPercentage": { name: "1st Serve Return Won %", dec: 1 },
+    "SecondServeReturnPointsWonPercentage": { name: "2nd Serve Return Won %", dec: 1 },
+    "ReturnPointsWonPercentage": { name: "Return Points Won %", dec: 1 },
+    "ReturnGamesWonPercentage": { name: "Return Games Won %", dec: 1 },
+    "AceAgainstPercentage": { name: "Opponent's Ace %", dec: 1 },
+    "BreakPointsConvertedPercentage": { name: "Break Points Converted %", dec: 1 }
 };
