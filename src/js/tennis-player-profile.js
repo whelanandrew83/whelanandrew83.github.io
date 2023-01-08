99fa50caab7f213fec561f19e097d91fece9e38e
whelanandrew83 <64817711+whelanandrew83@users.noreply.github.com>
2023-01-08 18:59:28 +1100

    Updates

--- src/js/tennis-player-profile.js
+++ src/js/tennis-player-profile.js
@@ -1,7 +1,11 @@
 const tableDataUrl = 'https://www.wheeloratings.com/src/player_profiles_tennis/table_data';
 
 const compareColumns = {
-    // "Age": { name: "Age", dec: 0 },
+    "Age": { name: "Age", dec: 0 },
     "Wins": { name: "Wins", dec: 0 },
-    "RacePoints": { name: "Race Points", dec: 1 }
+    "WinPercentage": { name: "Win %", dec: 1 },
+    "RacePoints": { name: "Race Points", dec: 1 },
+    "PointsWonPercentage": { name: "Points Won %", dec: 1 },
+    "GamesWonPercentage": { name: "Games Won %", dec: 1 },
+    "SetsWonPercentage": { name: "Sets Won %", dec: 1 }
 };
