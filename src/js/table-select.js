51f0a5e9a9d33587c419f2d42327615f4563e26d
whelanandrew83 <64817711+whelanandrew83@users.noreply.github.com>
2023-06-24 13:41:24 +1000

    Updates

--- src/js/table-select.js
+++ src/js/table-select.js
@@ -1,5 +1,9 @@
 let selectedRows = [];
 
 const selectRow = function (index) {
-    if (selectedRows.indexOf(index) < 0) selectedRows.push(index);
+    if (selectedRows.indexOf(index) < 0) {
+        selectedRows.push(index);
+    } else {
+        selectedRows.pop(index);
+    }
 }
