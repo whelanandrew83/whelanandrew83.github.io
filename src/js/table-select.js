bf5b284366e150afdaed19ef0188ea886d834b85
whelanandrew83 <64817711+whelanandrew83@users.noreply.github.com>
2023-06-24 16:47:10 +1000

    Updates

--- src/js/table-select.js
+++ src/js/table-select.js
@@ -4,6 +4,12 @@ const selectRow = function (index) {
     if (selectedRows.indexOf(index) < 0) {
         selectedRows.push(index);
     } else {
-        selectedRows.pop(index);
+        selectedRows.splice(selectedRows.indexOf(index), 1);
+    }
+}
+
+const rowStyle = function (index) {
+    if (selectedRows.indexOf(index) >= 0) {
+        return { backgroundColor: '#eee', boxShadow: 'inset 2px 0 0 0 #3a0057' }
     }
 }
