ab71bbad34cbd373ccc1c5ea2a82c13f4d77dbf6
whelanandrew83 <64817711+whelanandrew83@users.noreply.github.com>
2023-06-24 16:57:12 +1000

    Updates

--- src/js/table-select.js
+++ src/js/table-select.js
@@ -9,6 +9,7 @@ const selectRow = function (index) {
 }
 
 const rowStyle = function (index) {
+    return { backgroundColor: '#eee', boxShadow: 'inset 2px 0 0 0 #3a0057' };
     if (selectedRows.indexOf(index) >= 0) {
         return { backgroundColor: '#eee', boxShadow: 'inset 2px 0 0 0 #3a0057' }
     }
