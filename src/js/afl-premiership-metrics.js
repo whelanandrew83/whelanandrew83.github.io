337cb855ffb74d6e8e7cdd4dec6345c59396a71b
whelanandrew83 <64817711+whelanandrew83@users.noreply.github.com>
2026-01-07 17:50:11 +1100

    Updates

--- src/js/afl-premiership-metrics.js
+++ src/js/afl-premiership-metrics.js
@@ -2,10 +2,6 @@ const showCurrentTeamsButton = document.querySelector('#show-current');
 const showPremiersButton = document.querySelector('#show-premiers');
 const showAllTeamsButton = document.querySelector('#show-all');
 
-const updateView = function () {
-    Reactable.getInstance(reactableId).setMeta()
-}
-
 showCurrentTeamsButton.addEventListener('click', (e) => {
     Reactable.setAllFilters('premiership-metrics-table', [
         { id: 'IsCurrent', value: '1' },
@@ -46,14 +42,11 @@ showAllTeamsButton.addEventListener('click', (e) => {
 });
 
 window.addEventListener('DOMContentLoaded', (event) => {
+    if (typeof premiership_metrics !== 'undefined')
+        Reactable.getInstance(reactableId).setData(premiership_metrics);
+
     Reactable.setAllFilters('premiership-metrics-table', [
         { id: 'IsCurrent', value: '1' },
         { id: 'IsPremier', value: undefined }
     ])
-});
-
-const csvDownloadButton = document.querySelector('#download-csv-button');
-csvDownloadButton.addEventListener('click', (e) => {
-    Reactable.downloadDataCSV(reactableId, `${comp}-team-stats-${season}.csv`, { columnIds: [...new Set([...columnsAlwaysDisplayed, ...statsColumns.For, ...statsColumns.Against, ...statsColumns.Difference])].filter((el) => { return !missing_columns.includes(el) }) });
-    gtag('event', 'data_download');
 });
