e0ed91ed9b930951a3994bf71c0c32c69c2f8ed0
whelanandrew83 <64817711+whelanandrew83@users.noreply.github.com>
2026-01-08 19:00:22 +1100

    Updates

--- src/js/afl-premiership-metrics.js
+++ src/js/afl-premiership-metrics.js
@@ -43,12 +43,9 @@ showAllTeamsButton.addEventListener('click', (e) => {
 
 window.addEventListener('DOMContentLoaded', (event) => {
     if (typeof chart !== 'undefined') {
-        chart.update();
-        document.getElementById("temp-message").innerText = "Chart updated"
         window.setTimeout(() => {
-            document.getElementById("temp-message").innerText = "";
             chart.update();
-        }, 1000)
+        }, 200)
     }
 
     if (typeof premiership_metrics !== 'undefined')
